a9a8b3e2e16bfe2e7382b00cb473cf9b05abb4b8
SHANTANU SWARAJ <shantanu.9993223184@gmail.com>
2025-09-04 17:38:48 +0530

    python basic training day 4

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#a=10
+#b=5
+# this calculator.py file is an odule 
+#it's a python file containing functions, variables and classes
+def sum(a,b):
+    return a+b
+
+def difference(a,b):
+   return (a-b)
+
+def product(a,b):
+    return a*b
+
+def divison(a,b):
+    return (a/b)
---
+++ module_demo.py
@@ -0,0 +1,7 @@
+import calculator
+import math
+from math import sqrt
+import math as m
+#from calculator import add
+print(calculator.sum(50,40))
+print(calculator.product(5,4))
